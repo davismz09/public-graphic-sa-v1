35a0f92416a90ca1b6c5403c23b103d5f704750d
David Mite <dav.mite0907@gmail.com>
2024-02-11 18:52:52 -0500

    Fist Commit

---
+++ index.js
@@ -0,0 +1,54 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+// Menu
+const capa = document.getElementById("capaOpacity");
+const openMenu = document.querySelector(".buttonOpenMenu");
+const closeMenu = document.querySelector(".buttonCloseMenu");
+const containerMenu = document.querySelector(".containerMenuMobile");
+const menuMobile = document.querySelector(".menuMobile");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".menuMobile a");
+
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      // Remover la clase "active" de todos los elementos .menuMobile
+      const menuMobiles = document.querySelectorAll(".menuMobile");
+      menuMobiles.forEach((menuMobile) => {
+        containerMenu.classList.remove("active");
+        menuMobile.classList.remove("active");
+        capa.classList.remove("active");
+        openMenu.classList.remove("inactive");
+      });
+    });
+  });
+});
+
+openMenu.addEventListener("click", () => {
+  containerMenu.classList.add("active");
+  menuMobile.classList.add("active");
+  capa.classList.add("active");
+  openMenu.classList.add("inactive");
+});
+
+capa.addEventListener("click", () => {
+  containerMenu.classList.remove("active");
+  menuMobile.classList.remove("active");
+  capa.classList.remove("active");
+  openMenu.classList.remove("inactive");
+});
+
+closeMenu.addEventListener("click", () => {
+  containerMenu.classList.remove("active");
+  menuMobile.classList.remove("active");
+  capa.classList.remove("active");
+  openMenu.classList.remove("inactive");
+});
